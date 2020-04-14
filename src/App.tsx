6bc90d601cbc04e7639fe82af10ccf1757c5cd31
Evan Grim <evan@mirgnave.com>
2020-04-13 23:14:21 -0500

    Initial logins page

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,125 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import logins from './logins';
+
+const LoginsModal: React.FC = ({children}) => {
+  return <div>
+    <section role="dialog" tabIndex={-1}
+             className="slds-modal slds-fade-in-open slds-modal_medium"
+             aria-labelledby="header43" aria-modal="true" aria-describedby="modal-content-id-1">
+      <div className="slds-modal__container">
+        <header className="slds-modal__header slds-grid slds-grid_align-spread slds-grid_vertical-align-center">
+          <h2 id="header43" className="slds-text-heading_large">Salesforce Logins</h2>
+          <OpenAllButton/>
+        </header>
+        <div className="slds-modal__content slds-text-align--center slds-p-around_x-large">
+          {children}
+        </div>
+      </div>
+    </section>
+    <div className="slds-backdrop slds-backdrop_open"/>
+  </div>;
+};
+
+type TileProps = { name: string, link_url: string, img_url: string }
+
+function Tile({name, link_url, img_url}: TileProps) {
+  return (
+    <a href={link_url} className="slds-visual-picker slds-visual-picker_medium slds-text-link_reset">
+      <span className="slds-visual-picker__figure slds-visual-picker__icon slds-align_absolute-center slds-box_link">
+        <span className="slds-icon_container">
+          <img className="slds-icon slds-icon_large" src={img_url} aria-hidden="true" alt=""/>
+        </span>
+      </span>
+      <span className="slds-visual-picker__body slds-align_absolute-center">
+        <span className="slds-text-title">{name}</span>
+      </span>
+    </a>
+  )
+}
+
+function OpenAllButton() {
+  const [showPopover, setShowPopover] = useState(false);
+
+  let popover = null;
+  if (showPopover) {
+    popover =
+      <section aria-describedby="dialog-body-id-99" aria-labelledby="dialog-heading-id-1" id="popup-error-popover"
+               className="slds-popover slds-popover_error slds-nubbin_right-top-corner slds-is-absolute" role="dialog">
+        <button
+          className="slds-button slds-button_icon slds-button_icon-small slds-float_right slds-popover__close slds-button_icon-inverse"
+          title="Close dialog" onClick={event => {
+          event.preventDefault();
+          setShowPopover(false);
+        }}>
+          <svg className="slds-button__icon" aria-hidden="true">
+            <use xlinkHref={process.env.PUBLIC_URL + "/assets/icons/utility-sprite/svg/symbols.svg#close"}/>
+          </svg>
+          <span className="slds-assistive-text">Close dialog</span>
+        </button>
+        <header className="slds-popover__header">
+          <div className="slds-media slds-media_center slds-has-flexi-truncate ">
+            <div className="slds-media__figure">
+              <span className="slds-icon_container slds-icon-utility-error">
+                <svg className="slds-icon slds-icon_x-small" aria-hidden="true">
+                  <use xlinkHref={process.env.PUBLIC_URL + "/assets/icons/utility-sprite/svg/symbols.svg#error"}/>
+                </svg>
+              </span>
+            </div>
+            <div className="slds-media__body">
+              <h2 className="slds-truncate slds-text-heading_medium" id="dialog-heading-id-1"
+                  title="Resolve error">Grumpy Browser</h2>
+            </div>
+          </div>
+        </header>
+        <div className="slds-popover__body" id="dialog-body-id-99">
+          <p>Your browser prevented us from opening all these login pages.
+            Please try disabling your popup-blocker and try again.
+          </p>
+        </div>
+      </section>
+  }
+
+  return (
+    <div className="slds-is-relative">
+      {popover}
+      <button className="slds-button slds-button_neutral" onClick={(event) => {
+        event.preventDefault();
+        setShowPopover(false);
+
+        let pickerElements = document.querySelectorAll<HTMLAnchorElement>('a.slds-visual-picker');
+        let windowRefs: Window[] = [];
+        for (let i = 0; i < pickerElements.length; i++) {
+          let pickerElement = pickerElements[i];
+          let windowRef = window.open(pickerElement.href);
+          if (windowRef == null) {
+            console.error("Failed to open a link (likely due to a pop-up blocker)");
+            setShowPopover(true);
+            windowRefs.map(windowRef => windowRef.close());
+            return;
+          }
+
+          windowRefs.push(windowRef);
+        }
+
+        windowRefs[0].focus();
+      }}>
+        <svg className="slds-button__icon slds-button__icon_left">
+          <use xlinkHref={process.env.PUBLIC_URL + "/assets/icons/utility-sprite/svg/symbols.svg#open"}/>
+        </svg>
+        Open All
+      </button>
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <LoginsModal>
+      {logins.map(login_data => <Tile name={login_data.name} link_url={login_data.link_url} key={login_data.link_url}
+                                      img_url={login_data.img_url}/>)}
+    </LoginsModal>
+  )
 }
 
 export default App;
---
+++ src/logins.tsx
@@ -0,0 +1,84 @@
+import sales from "./img/sales.svg";
+import marketing from "./img/marketing.svg";
+import commerce from "./img/commerce.svg";
+import heroku from "./img/heroku.svg";
+import mulesoft from "./img/mulesoft.svg";
+import tableau from "./img/tableau.png";
+import trailblazer from "./img/trailhead.svg";
+import quip from "./img/quip.svg";
+import service from "./img/service.svg";
+
+export const logins = [
+  {
+    "name": "Sales & Service Cloud",
+    "link_url": "https://login.salesforce.com",
+    "img_url": sales
+  },
+  {
+    "name": "Marketing Cloud",
+    "link_url": "https://mc.login.exacttarget.com/hub-cas/login",
+    "img_url": marketing
+  },
+  {
+    "name": "Pardot",
+    "link_url": "https://pi.pardot.com/",
+    "img_url": marketing
+  },
+  {
+    "name": "Audience Studio",
+    "link_url": "https://console.krux.com/login",
+    "img_url": marketing
+  },
+  {
+    "name": "Social Studio",
+    "link_url": "https://socialstudio.radian6.com/login",
+    "img_url": marketing
+  },
+  {
+    "name": "Rebel",
+    "link_url": "https://dashboard.rebelmail.com/login",
+    "img_url": marketing
+  },
+  {
+    "name": "Datorama",
+    "link_url": "https://platform.datorama.com/login",
+    "img_url": marketing
+  },
+  {
+    "name": "Commerce Cloud",
+    "link_url": "https://account.demandware.com/",
+    "img_url": commerce
+  },
+  {
+    "name": "Heroku",
+    "link_url": "https://id.heroku.com",
+    "img_url": heroku
+  },
+  {
+    "name": "Mulesoft",
+    "link_url": "https://anypoint.mulesoft.com/login/",
+    "img_url": mulesoft
+  },
+  {
+    "name": "Tableau",
+    "link_url": "https://sso.online.tableau.com/public/login",
+    "img_url": tableau
+  },
+  {
+    "name": "Trailhead",
+    "link_url": "https://trailblazer.me/trailblazerlogin?&locale=en_US",
+    "img_url": trailblazer
+  },
+  {
+    "name": "Quip",
+    "link_url": "https://quip.com/account/login",
+    "img_url": quip
+  },
+  {
+    "name": "Click",
+    "link_url": "https://login.clicksoftware.com/",
+    "img_url": service
+  },
+]
+
+export default logins;
