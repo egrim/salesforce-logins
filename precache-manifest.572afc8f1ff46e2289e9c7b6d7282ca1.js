ccb7837b2c4185f6a5b8778ad75ac079a3addc5e
Evan Grim <evan@mirgnave.com>
2020-04-14 18:01:28 -0500

    Updates

---
+++ precache-manifest.572afc8f1ff46e2289e9c7b6d7282ca1.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b27950d4e86132d25a67e47d1eccac2",
+    "url": "/salesforce-logins/index.html"
+  },
+  {
+    "revision": "30b820287ba7e1e6c2a6",
+    "url": "/salesforce-logins/static/css/main.43dbb3eb.chunk.css"
+  },
+  {
+    "revision": "09696518a86828b8bdd0",
+    "url": "/salesforce-logins/static/js/2.9b37b56a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/salesforce-logins/static/js/2.9b37b56a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30b820287ba7e1e6c2a6",
+    "url": "/salesforce-logins/static/js/main.af39c07f.chunk.js"
+  },
+  {
+    "revision": "8aa202ce68cea69ad7d8",
+    "url": "/salesforce-logins/static/js/runtime-main.b9f8f19a.js"
+  },
+  {
+    "revision": "ca639af6daed830e6892bbef32080c9d",
+    "url": "/salesforce-logins/static/media/commerce.ca639af6.svg"
+  },
+  {
+    "revision": "813b8a0a5cbbbb3610e255d83f8d8be2",
+    "url": "/salesforce-logins/static/media/heroku.813b8a0a.svg"
+  },
+  {
+    "revision": "aee61162638e2ac79832f552bff3c208",
+    "url": "/salesforce-logins/static/media/marketing.aee61162.svg"
+  },
+  {
+    "revision": "856b39f16630de3aa413a45821c48dcb",
+    "url": "/salesforce-logins/static/media/mulesoft.856b39f1.svg"
+  },
+  {
+    "revision": "47eaff87f220dbc5605392e075533d86",
+    "url": "/salesforce-logins/static/media/quip.47eaff87.svg"
+  },
+  {
+    "revision": "daefda226fd744998f9b1f6328e70b75",
+    "url": "/salesforce-logins/static/media/sales.daefda22.svg"
+  },
+  {
+    "revision": "19ea744e39e5386413e48dce74a4282b",
+    "url": "/salesforce-logins/static/media/service.19ea744e.svg"
+  },
+  {
+    "revision": "03471518856a789723efd9a1e159790f",
+    "url": "/salesforce-logins/static/media/trailhead.03471518.svg"
+  }
+]);
